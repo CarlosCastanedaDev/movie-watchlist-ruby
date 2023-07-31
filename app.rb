07464b7e658eb4adbcbb96fe57646faa846deafd
Carlos Castaneda <101298482+CarlosCastanedaDev@users.noreply.github.com>
2023-07-31 21:35:46 +0000

    fix trending list links

--- app.rb
+++ app.rb
@@ -54,6 +54,32 @@ poster = @res.dig("results", 0, "poster_path")
 erb(:list)
 end
 
-get("trending_movie") do
+get("/trending_movie/:title") do
+    movie = params.fetch(:title)
+ 
+ # Taken from TMDB Api Docs
+ url = URI("https://api.themoviedb.org/3/search/movie?query=#{movie}")
+ 
+ http = Net::HTTP.new(url.host, url.port)
+ http.use_ssl = true
+ 
+ request = Net::HTTP::Get.new(url)
+ request["accept"] = 'application/json'
+ request["Authorization"] = "Bearer #{token}"
+ 
+ response = http.request(request)
+ parsed_response = JSON.parse(response.read_body)
+ 
+ 
+ @res = parsed_response
+ @title = @res.dig("results" ,0, "original_title")
+ poster = @res.dig("results", 0, "poster_path")
+ @overview = @res.dig("results" ,0, "overview")
+ @release_date = @res.dig("results" ,0, "release_date")
+ @avg_rating = @res.dig("results" ,0, "vote_average")
+ @id = @res.dig("results", 0, "id")
+ @image_url = "https://image.tmdb.org/t/p/w300/#{poster}"
+ 
+ 
   erb(:list)
 end
