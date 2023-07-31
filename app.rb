e508e98e28a78e711d2fc9041c8329fb73030b71
Carlos Castaneda <101298482+CarlosCastanedaDev@users.noreply.github.com>
2023-07-31 20:18:07 +0000

    cleaned up code

--- app.rb
+++ app.rb
@@ -1,6 +1,5 @@
 require "sinatra"
 require "sinatra/reloader"
-require "http"
 require 'uri'
 require 'net/http'
 require "sinatra/cookies"
@@ -13,7 +12,7 @@ get("/") do
   erb(:home)
 end
 
-get("/movielist") do
+get("/movie") do
    movie = params.fetch("movie")
 
 # Taken from TMDB Api Docs
@@ -40,38 +39,4 @@ poster = @res.dig("results", 0, "poster_path")
 @image_url = "https://image.tmdb.org/t/p/w300/#{poster}"
 
 erb(:list)
-end
-
-post("/add_to_watchlist/:id/:title") do
-  title = params[:title]
-  watchlist = request.cookies['watchlist']
-
-  if watchlist.nil?
-    # If the cookie doesn't exist, create a new array with the movie title
-    watchlist = [title]
-  else
-    # If the cookie exists, parse the JSON and add the movie title to the array
-    watchlist = JSON.parse(watchlist)
-    watchlist.push(title)
-  end
-
-  # Save the updated watchlist back to the cookie
-  response.set_cookie('watchlist', value: watchlist.to_json, expires: Time.now + 3600)
-
-  # Redirect the user to the home page or wherever appropriate
-  # redirect("/my_watchlist")
-   erb(:watchlist)
-end
-
-get("/my_watchlist") do
-  watchlist = request.cookies['watchlist']
-
-  # Check if the 'watchlist' cookie exists and parse it as a JSON array
-  if watchlist.nil?
-    @movies = []
-  else
-    @movies = JSON.parse(watchlist)
-  end
-
-  erb(:my_watchlist)
 end
